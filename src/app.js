e0d8b25cd35333649d50c6b51a14a8dbe1cc97e9
WiscasHubble <luis.acevedosl@uanl.edu.mx>
2024-11-11 00:57:38 -0600

    Funciones para la obtencion de recomendaciones

--- src/app.js
+++ src/app.js
@@ -1,33 +1,131 @@
-const parrafoRecomendaciones = document.getElementById('cardRecomendaciones');
-function obtenerRecomendacionNecesidad(storedNecesidad) {
-    
-    return (storedNecesidad <= 25) ? parrafoRecomendaciones.textContent = 'Dedicar tiempo a alguna actividad recreativa de interes personal (hobbies).' :
-           (storedNecesidad <= 50) ? parrafoRecomendaciones.textContent = 'Meditacion.' :
-           (storedNecesidad <= 75) ? parrafoRecomendaciones.textContent = 'Invertir grandes cantidades de tiempo y esfuerzo en actividades fisicas.' :
-           (storedNecesidad <= 100) ? parrafoRecomendaciones.textContent = 'Agendar cita con un profesional.' :
-           'Valor no válido.';
+
+
+function obtenerRecomendacionesSatisfaccion(puntuacion) {
+    if (puntuacion > 80) {
+        return {
+            frase: "La persona se siente más cómoda y satisfecha interactuando en redes sociales que en la vida real.",
+            estructuraHtml: `
+                <blockquote class="text-lg italic font-semibold text-blue-500 dark:text-white">
+                    <p>"La persona se siente más cómoda y satisfecha interactuando en redes sociales que en la vida real."</p>
+                </blockquote>
+                `
+        };
+    } else if (puntuacion > 40) {
+        return {
+            frase: "La persona encuentra una satisfacción notable en las redes, a veces prefiriendo las interacciones virtuales.",
+            estructuraHtml: `
+                <blockquote class="text-lg italic font-semibold text-blue-500 dark:text-white">
+                    <p>"La persona encuentra una satisfacción notable en las redes, a veces prefiriendo las interacciones virtuales."</p>
+                </blockquote>
+                `
+        };
+    } else {
+        return {
+            frase: "La persona disfruta de las redes sociales, pero no las necesita para sentirse bien.",
+            estructuraHtml: `
+                <blockquote class="text-lg italic font-semibold text-blue-500 dark:text-white">
+                    <p>"La persona disfruta de las redes sociales, pero no las necesita para sentirse bien."</p>
+                </blockquote>
+                `
+        };
+    }
 }
 
-function obtenerRecomendacionObsesion(storedObsesion) {
-    return (storedObsesion <= 25) ? parrafoRecomendaciones.textContent = 'Prescindir de los dispositivos unicamente en momentos clave o estrictamente necesarios.' :
-           (storedObsesion <= 50) ? parrafoRecomendaciones.textContent = 'Establecer un tiempo minimo de 15 minutos entre conexiones a redes.' :
-           (storedObsesion <= 75) ? parrafoRecomendaciones.textContent = 'Explorar el miedo al "FOMO"(miedo a perderse algo).' :
-           (storedObsesion <= 100) ? parrafoRecomendaciones.textContent = 'Trabajar en la autoestima y la validacion interna.' :
-           'Valor no válido.';
+
+
+function obtenerRecomendacionesProblemas(puntuacion) {
+    if (puntuacion > 80) {
+        return {
+            frase: "Es probable que estés pasando mucho tiempo en redes sociales, lo cual podría estar afectando tu bienestar y tu vida social fuera de internet. Sería útil que consideraras equilibrar tu tiempo entre las redes sociales y otras actividades.",
+            estructuraHtml: `
+                <blockquote class="text-lg italic font-semibold text-red-500 dark:text-white">
+                    <p>"Es probable que estés pasando mucho tiempo en redes sociales, lo cual podría estar afectando tu bienestar y tu vida social fuera de internet. Sería útil que consideraras equilibrar tu tiempo entre las redes sociales y otras actividades."</p>
+                </blockquote>
+                `
+        };
+    } else if (puntuacion > 40) {
+        return {
+            frase: "Parece que el uso de redes sociales está teniendo un impacto notable en tu vida. Aunque no es algo extremo, sería conveniente reflexionar sobre cómo este uso afecta tu vida diaria y tratar de gestionar mejor tu tiempo online.",
+            estructuraHtml: `
+                <blockquote class="text-lg italic font-semibold text-red-500 dark:text-white">
+                    <p>"Parece que el uso de redes sociales está teniendo un impacto notable en tu vida. Aunque no es algo extremo, sería conveniente reflexionar sobre cómo este uso afecta tu vida diaria y tratar de gestionar mejor tu tiempo online."</p>
+                </blockquote>
+                `
+        };
+    } else {
+        return {
+            frase: "El uso de redes sociales no parece estar teniendo un gran impacto en tu vida diaria. Aún así, es importante mantener un balance para asegurarte de que no se convierta en una distracción mayor.",
+            estructuraHtml: `
+                <blockquote class="text-lg italic font-semibold text-red-500 dark:text-white">
+                    <p>"El uso de redes sociales no parece estar teniendo un gran impacto en tu vida diaria. Aún así, es importante mantener un balance para asegurarte de que no se convierta en una distracción mayor."</p>
+                </blockquote>
+                `
+        };
+    }
 }
 
-function obtenerRecomendacionProblemas(storedProblemas) {
-    return (storedProblemas <= 25) ? parrafoRecomendaciones.textContent = 'Proporcionar información sobre los niveles de dependencia a redes.' :
-           (storedProblemas <= 50) ? parrafoRecomendaciones.textContent = 'Definir un tiempo mínimo diario para realizar actividades fuera de redes sociales.' :
-           (storedProblemas <= 75) ? parrafoRecomendaciones.textContent = 'Crear y establecer una rutina enfocada en aspectos personales.' :
-           (storedProblemas <= 100) ? parrafoRecomendaciones.textContent = 'Crear y establecer una rutina enfocada en aspectos personales.' :
-           'Valor no válido.';
+
+
+function obtenerRecomendacionesObsesion(puntuacion) {
+    if (puntuacion > 80) {
+        return {
+            frase: "Parece que la necesidad de estar constantemente conectado a las redes sociales está afectando tu bienestar. Es importante que tomes un paso atrás y busques un equilibrio entre tu vida online y offline para evitar que esta obsesión interfiera en otros aspectos de tu vida.",
+            estructuraHtml: `
+                <blockquote class="text-lg italic font-semibold text-green-500 dark:text-white">
+                    <p>"Parece que la necesidad de estar constantemente conectado a las redes sociales está afectando tu bienestar. Es importante que tomes un paso atrás y busques un equilibrio entre tu vida online y offline para evitar que esta obsesión interfiera en otros aspectos de tu vida."</p>
+                </blockquote>
+                `
+        };
+    } else if (puntuacion > 40) {
+        return {
+            frase: "Tu conexión constante a las redes sociales parece estar siendo una parte importante de tu vida. Aunque no es algo excesivo, sería útil reflexionar sobre cómo afecta tus interacciones personales y buscar momentos para desconectar y disfrutar de actividades fuera de la pantalla.",
+            estructuraHtml: `
+                <blockquote class="text-lg italic font-semibold text-green-500 dark:text-white">
+                    <p>"Tu conexión constante a las redes sociales parece estar siendo una parte importante de tu vida. Aunque no es algo excesivo, sería útil reflexionar sobre cómo afecta tus interacciones personales y buscar momentos para desconectar y disfrutar de actividades fuera de la pantalla."</p>
+                </blockquote>
+                `
+        };
+    } else {
+        return {
+            frase: "Parece que tu relación con las redes sociales no está afectando en gran medida tu vida diaria. Sin embargo, sigue siendo importante mantener un equilibrio para evitar que esta dependencia aumente con el tiempo.",
+            estructuraHtml: `
+                <blockquote class="text-lg italic font-semibold text-green-500 dark:text-white">
+                    <p>"Parece que tu relación con las redes sociales no está afectando en gran medida tu vida diaria. Sin embargo, sigue siendo importante mantener un equilibrio para evitar que esta dependencia aumente con el tiempo."</p>
+                </blockquote>
+                `
+        };
+    }
 }
 
-function obtenerRecomendacionSatisfaccion(storedSatisfaccion) {
-    return (storedSatisfaccion <= 25) ? parrafoRecomendaciones.textContent = 'Involucrarse en actividades recreativas que generen satisfacción personal por logros.' :
-           (storedSatisfaccion <= 50) ? parrafoRecomendaciones.textContent = 'Pasar más tiempo con la familia.' :
-           (storedSatisfaccion <= 75) ? parrafoRecomendaciones.textContent = 'Hacer actividades con amigos o relaciones personales.' :
-           (storedSatisfaccion <= 100) ? parrafoRecomendaciones.textContent = 'Explorar experiencias significativas fuera de las redes sociales.' :
-           'Valor no válido.';
+
+
+function obtenerRecomendacionesNecesidad(puntuacion) {
+    if (puntuacion > 80) {
+        return {
+            frase: "Parece que la necesidad de estar constantemente conectado a las redes sociales está tomando un papel central en tu vida. Es importante reflexionar sobre el impacto que esto puede tener en tu bienestar emocional y en las relaciones personales. Considera buscar momentos para desconectar y reconectar con el mundo offline.",
+            estructuraHtml: `
+                <blockquote class="text-lg italic font-semibold text-yellow-500 dark:text-white">
+                    <p>"Parece que la necesidad de estar constantemente conectado a las redes sociales está tomando un papel central en tu vida. Es importante reflexionar sobre el impacto que esto puede tener en tu bienestar emocional y en las relaciones personales. Considera buscar momentos para desconectar y reconectar con el mundo offline."</p>
+                </blockquote>
+                `
+        };
+    } else if (puntuacion > 40) {
+        return {
+            frase: "Tu necesidad de estar conectado a las redes sociales es moderada, pero aún puede tener un impacto en tu bienestar. Reflexiona sobre cómo tu uso de redes sociales afecta tu vida diaria y busca un balance para no depender excesivamente de ellas.",
+            estructuraHtml: `
+                <blockquote class="text-lg italic font-semibold text-yellow-500 dark:text-white">
+                    <p>"Tu necesidad de estar conectado a las redes sociales es moderada, pero aún puede tener un impacto en tu bienestar. Reflexiona sobre cómo tu uso de redes sociales afecta tu vida diaria y busca un balance para no depender excesivamente de ellas."</p>
+                </blockquote>
+                `
+        };
+    } else {
+        return {
+            frase: "Parece que no tienes una necesidad urgente de estar constantemente conectado a las redes sociales. Sin embargo, sigue siendo importante mantener un equilibrio saludable, ya que el exceso de conexión puede surgir de manera gradual.",
+            estructuraHtml: `
+                <blockquote class="text-lg italic font-semibold text-yellow-500 dark:text-white">
+                    <p>"Parece que no tienes una necesidad urgente de estar constantemente conectado a las redes sociales. Sin embargo, sigue siendo importante mantener un equilibrio saludable, ya que el exceso de conexión puede surgir de manera gradual."</p>
+                </blockquote>
+                `
+        };
+    }
 }
